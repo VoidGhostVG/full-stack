16f7d9c48baac2b5be3875d14e8d568b441f4859
VoidGhostVG <vluiz0196@gmail.com>
2025-11-10 15:09:00 -0300

    Arquivos JavaScript

---
+++ logica-programacao/primeiro.js
@@ -0,0 +1,7 @@
+const userName = prompt("Qual é o seu nome?");
+
+const userAge = prompt("Qual é a sua idade?");
+
+document.write("Seu nome é: " + "<strong>" + userName + "</strong>");
+
+document.write("Sua idade é: " + "<strong>" + userAge + "</strong>");
---
+++ logica-programacao/segundo.js
@@ -0,0 +1,4 @@
+let numero1 = prompt("digite um número");
+let numero2 = prompt("digite outro número");
+
+console.log (numero1 + numero2)
