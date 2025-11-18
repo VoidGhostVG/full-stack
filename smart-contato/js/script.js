2ba18d4c24940da3939f2ffe8b2465c0d65f2a0e
VoidGhostVG <vluiz0196@gmail.com>
2025-11-18 16:15:11 -0300

    18/11

--- smart-contato/js/script.js
+++ smart-contato/js/script.js
@@ -1,10 +1,29 @@
+const form = document.querySelector("form")
+const lista = document.querySelector(".lista")
 const inputNome = document.getElementById("nome")
 const inputEmail = document.getElementById("email")
 const inputTel = document.getElementById("telefone")
 
-function botao(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault()
+
+   if(inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
+    alert("Ainda há algo para preencher!")
+    return false
+}
+
     console.log(inputNome.value)
     console.log(inputEmail.value)
     console.log(inputTel.value)
-}
+
+    const li = document.createElement("li")
+lista.appendChild(li)
+})
+
+
+
+li.innerHTML = `
+  <span class="contato-nome">${inputNome.value}</span>
+  <span class="contato-email">${inputEmail.value}</span>
+  <span class="contato-telefone">${inputTel.value}</span>
+`
