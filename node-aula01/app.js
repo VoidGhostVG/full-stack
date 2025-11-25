08201c493d3f2ea9effbf9e26e9a1bb506613a5b
VoidGhostVG <vluiz0196@gmail.com>
2025-11-25 14:57:19 -0300

    Primeiros requests dentro da collection

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,12 +2,19 @@ const express  = require("express");
 const app = express();
 const port = 3000;
 
+
 // Variável com os dados produtos
 const produtos = [
     { id: 1, nome: 'Produto A', preco: 100 },
     { id: 2, nome: 'Produto B', preco: 150 },
     { id: 3, nome: 'Produto C', preco: 200 },
 ];
+
+const times = [
+    {id: 1, nome: 'Santos', ano: 1912},
+    {id: 2, nome: 'Mirassol', ano: 1910},
+    {id: 3, nome: 'Bangu', ano: 1904}
+]
 
 // Criar função
 // Rota principal
@@ -24,6 +31,11 @@ app.get('/produtos', (req, res) => {
 app.get('/home', (req, res) => {
     //console.log("ola: ",__dirname);
     res.sendFile(__dirname + '/public/home.html');
+});
+
+// Rota times
+app.get('/times', (req, res) => {
+    res.send(times);
 });
 
 // Executando o sevidor
