cd13d1a547e0200a94fd7cdd2d2916b78ed92a69
VoidGhostVG <vluiz0196@gmail.com>
2025-11-25 16:27:44 -0300

    25/11

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,6 +2,8 @@ const express  = require("express");
 const app = express();
 const port = 3000;
 
+// Middleware para servir arquivos estáticos
+app.use(express.static('public'));
 
 // Variável com os dados produtos
 const produtos = [
