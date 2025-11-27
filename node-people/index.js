db8f62f4fc83760bb153c3005cb46906993cb586
VoidGhostVG <vluiz0196@gmail.com>
2025-11-27 16:23:25 -0300

    27/11

--- node-people/index.js
+++ node-people/index.js
@@ -14,15 +14,33 @@ const nomes = [
   { id: 5, nome: "Doris", idade: "33" },
 ];
 
+const times = [
+  { id: 1, nome: "Corinthians", estado: "SP", titulos: 7 },
+  { id: 2, nome: "Palmeiras", estado: "SP", titulos: 11 },
+  { id: 3, nome: "Santos", estado: "SP", titulos: 8 },
+  { id: 4, nome: "Flamengo", estado: "RJ", titulos: 7 },
+  { id: 5, nome: "Vasco", estado: "RJ", titulos: 4 },
+  { id: 6, nome: "Atlético Mineiro", estado: "MG", titulos: 3 },
+  { id: 7, nome: "Cruzeiro", estado: "MG", titulos: 4 },
+]
+
 // função auxiliar
 // retornar o Id
 function buscarNomePorId(id) {
 return nomes.filter((nome) => nome.id == id)
 }
 
+function buscarTimePorId(id) {
+return times.filter((time) => time.id == id)
+}
+
 // pegar a posição do elemento do array por id
 function buscarIdNomes(id) {
     return nomes.findIndex((nome) => nome.id == id)
+}
+
+function buscarIdTimes(id) {
+    return times.findIndex((time) => time.id == id)
 }
 
 app.listen(PORT, () => {
@@ -58,4 +76,24 @@ app.get("/teste", (req, res) => {
     app.post("/nomes", (req, res) => {
         nomes.push(req.body)
         res.status(201).send("Nome cadastrado com sucesso")
+    })
+
+    app.get("/times", (req, res) => {
+        res.send(times)
+    })
+
+    app.get("/times/:id", (req, res) =>{
+        let index = req.params.id
+        res.json(buscarTimePorId(index))
+    })
+
+    app.delete("/times/:id", (req, res) => {
+        let index = buscarIdTimes(req.params.id)
+        times.splice(index, 1)
+        res.send("Time(s) com id ${req.params.id} excluído(s) com sucesso!")
+    })
+
+    app.post("/times", (req, res) => {
+        times.push(req.body)
+        res.status(201).send("Seus times foram cadastrados!")
     })
